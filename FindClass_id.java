4440151002c92fdd7222b8f9d3d51ece5052e682
Kaustubha_Pundir <kaustubhapundir@gmail.com>
2025-07-01 12:00:49 +0530

    Create FindClass_id.java

---
+++ FindClass_id.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(peakIndexInMountainArray(arr));
+    }
+
+    // Function declaration
+    public static int peakIndexInMountainArray(int[] A) {
+        int req_class=0;
+        for(int i=0;i<A.length;i++){
+            if(i==0){
+                if(A[i+1]<=A[i]){
+                    req_class=i;
+                    break;
+                }
+            }
+            else if(i==A.length-1){
+                if(A[i-1]<=A[i]){
+                    req_class=i;
+                    break;
+                }
+            }
+            else{
+                if(A[i-1]<=A[i] && A[i+1]<=A[i]){
+                    req_class=i;
+                    break;
+                }
+            }
+        }
+        return req_class;
+    }
+}
