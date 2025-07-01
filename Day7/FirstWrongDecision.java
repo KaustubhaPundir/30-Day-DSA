8fab24cdb1ded79e07fb1fd848136e9e4cb91d7e
Kaustubha_Pundir <kaustubhapundir@gmail.com>
2025-07-01 12:07:45 +0530

    Create FirstWrongDecision.java

---
+++ Day7/FirstWrongDecision.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int FirstWrongDecision(String s) {
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='W'){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+        // Call the function FirstWrongDecision().
+        System.out.println(FirstWrongDecision(str));
+    }
+}
